3e54e347537f40e3f74f7e70305d491cf136219e
Scriper.irk <scriper.irk@gmail.com>
2023-01-17 00:19:54 +0800

    Step_1

--- gh.test.js
+++ gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,18 +9,22 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("http://github.com/team")
+  }, 10000);
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
-  });
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub')
+  }, 40000);
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
-    expect(actual).toEqual("#start-of-content");
-  });
+    const actual = await page.$eval("a", link => link.getAttribute('href'));
+    expect(actual).toEqual("#start-of-content")
+  }, 40000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
@@ -29,6 +32,26 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
+    expect(actual).toContain("Sign up for free", {timeout: 60000})
   });
+});
+
+describe("Should check titles for GitHub pages", () => {
+  test("Should check Pricing page", async () => {
+    await page.goto("https://github.com/pricing");
+    const title = await page.title();
+    expect(title).toContain("Pricing · Plans for every developer · GitHub")
+  }, 60000);
+
+  test("Should check About page", async () => {
+    await page.goto("https://github.com/about");
+    const title = await page.title();
+    expect(title).toContain("About · GitHub")
+  }, 60000);
+
+  test("Should check Shop page", async () => {
+    await page.goto("https://github.com/marketplace");
+    const title = await page.title();
+    expect(title).toContain("GitHub Marketplace · to improve your workflow · GitHub")
+  }, 60000);
 });
